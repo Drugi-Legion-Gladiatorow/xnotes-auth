c8c4a650b145e06123721cba42e86ace48651179
Bartek <bartek@iMac.local>
2020-08-26 19:49:15 +0200

    stop touching my shit thanks

--- src/app.ts
+++ src/app.ts
@@ -1,31 +1,33 @@
-import express, { Application } from "express"
-import path from "path"
-import bodyParser from "body-parser"
-import { writeFileSync } from "fs"
-import router from "./routes"
-import connect from "./db/connection"
+import express, { Application } from 'express';
+import path from 'path';
+import bodyParser from 'body-parser';
+import { writeFileSync } from 'fs';
+import router from './routes';
+import connect from './db/connection';
 
-const ip = require("ip")
-const passport = require("./passport/passport")
-const cors = require("cors")
+const ip = require('ip');
+const passport = require('./passport/passport');
+const cors = require('cors');
 
 function onInit() {
-  const app: Application = express()
-  app.use(bodyParser.json())
-  app.use(cors())
+  const app: Application = express();
+  app.use(bodyParser.json());
+  app.use(cors());
 
-  app.use(express.static(path.join(__dirname, "public")))
+  app.use(express.static(path.join(__dirname, 'public')));
 
-  writeFileSync("./shared/auth.json", JSON.stringify({ ip: ip.address() }))
+  writeFileSync('./shared/auth.json', JSON.stringify({ ip: ip.address() }));
 
-  connect()
-  app.use(passport.initialize())
+  connect();
+  app.use(passport.initialize());
 
-  app.use(router)
+  app.use(router);
 
   app.listen(process.env.PORT || 3000, () => {
-    console.log(`auth service is listening at port ${process.env.PORT || 3000}!`)
-  })
+    console.log(
+      `auth service is listening at port ${process.env.PORT || 3000}!`
+    );
+  });
 }
 
-onInit()
+onInit();
